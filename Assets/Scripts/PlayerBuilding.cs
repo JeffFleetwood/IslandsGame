4695cbd8238e56e56af46e74d56c427914ad4747
hectorborges <hectorborges94@gmail.com>
2020-06-12 10:11:25 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerBuilding.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBuilding : MonoBehaviour
+{
+    public List<GameObject> buildingOptions;
+    public float rayDistance = 100;
+    public float rotateSpeed = 5f;
+    public LayerMask environmentLayer;
+
+    public float smoothSpeed = 75f;
+
+    Vector3 rotation;
+
+    int lastSelection;
+    int currentSelection;
+
+    float scrollWheel;
+
+    bool buildingModeActive;
+    bool useNormal = true;
+
+    Camera cam;
+    GameObject currentSelectionPreview;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        currentSelectionPreview = Instantiate(buildingOptions[0]);
+        currentSelectionPreview.gameObject.layer = LayerMask.NameToLayer("Default");
+        currentSelectionPreview.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            buildingModeActive = !buildingModeActive;
+
+        currentSelectionPreview.gameObject.SetActive(currentSelectionPreview && buildingModeActive);
+        if (!buildingModeActive) return;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+            useNormal = !useNormal;
+           
+        ChangePreviewPiece();
+        CreateBuildPiece();
+    }
+
+    void ChangePreviewPiece()  //Changes the currently selected piece with MouseWheel and creates a new preview of it.
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentSelection + 1 >= buildingOptions.Count)
+                currentSelection = 0;
+            else
+                currentSelection++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (currentSelection - 1 < 0)
+                currentSelection = buildingOptions.Count - 1;
+            else
+                currentSelection--;
+        }
+
+        if (currentSelection != lastSelection && currentSelectionPreview)
+        {
+            lastSelection = currentSelection;
+            Destroy(currentSelectionPreview);
+            currentSelectionPreview = Instantiate(buildingOptions[currentSelection].gameObject);
+            currentSelectionPreview.gameObject.layer = LayerMask.NameToLayer("Default");
+        }
+    }
+
+    void CreateBuildPiece()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out hit, rayDistance, environmentLayer))
+        {
+            if (currentSelectionPreview)
+            {
+                currentSelectionPreview.transform.position = hit.point;
+
+                if (useNormal)
+                    currentSelectionPreview.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                else
+                    currentSelectionPreview.transform.rotation = Quaternion.Euler(Vector3.up);
+
+                RotatePiece();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                Instantiate(buildingOptions[currentSelection], hit.point, currentSelectionPreview.transform.rotation);
+        }
+    }
+
+    void RotatePiece()
+    {
+        scrollWheel += Input.mouseScrollDelta.y;
+
+        if(currentSelectionPreview)
+            currentSelectionPreview.transform.Rotate(Vector3.up, scrollWheel * rotateSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotationSpeed = 1;
+    public float gravity = 35;
+    public float jumpForce = 25;
+    public LayerMask groundLayer;
+
+    float mouseX;
+    float mouseY;
+
+    float horizontal;
+    float vertical;
+
+    float verticalVelocity;
+
+    Camera cam;
+    CharacterController cc;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        cc = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        LockCursor();
+        Jump();
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horazontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horazontal, 0, vertical);
+        movement = transform.TransformDirection(movement);
+        movement.y = 0;
+
+        Vector3 move = new Vector3(movement.x * speed, verticalVelocity, movement.z * speed);
+        cc.Move(move * Time.deltaTime);
+    }
+
+    void Rotate()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        mouseY += Input.GetAxis("Mouse Y");
+
+        mouseX *= rotationSpeed;
+        mouseY *= rotationSpeed;
+
+        mouseY = Mathf.Clamp(mouseY, -60f, 90f);
+
+        transform.rotation = Quaternion.Euler(0, mouseX, 0);
+        cam.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0);
+    }
+
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                verticalVelocity = jumpForce;
+        }
+        else
+            verticalVelocity -= gravity * Time.deltaTime;
+    }
+
+    void LockCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+
+            Cursor.visible = !Cursor.visible;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(transform.position, .1f, groundLayer);
+    }
+}
