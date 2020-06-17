3c58bfd85d689b8d0464fc1738ac9b8aed49710c
pupdog5089 <61168201+pupdog5089@users.noreply.github.com>
2020-06-17 14:55:30 -0500

    I learned how to learn new scripts.

---
+++ Assets/Scripts/IslandMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IslandMenu : MonoBehaviour
+{
+    public Image displayimage;
+    public Sprite[] IslandImages;
+    public GameObject[] IslandTemplates;
+    public int index;
+
+    public void Start() 
+    {
+        displayimage.sprite = IslandImages[0];
+}
+    public void PreviousIsland() 
+    {
+        if (index > 0)
+        {
+            index -= 1;
+        }
+        displayimage.sprite = IslandImages[index];
+    }
+    public void NextIsland()
+    {
+        if (index < IslandImages.Length -1)
+        {
+            index += 1;
+        }
+        displayimage.sprite = IslandImages[index]; 
+    }
+
+    public void SelectIsland()
+    {
+        Debug.Log("Works");
+        IslandTemplates[index].SetActive(true);
+    }
+}
---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyScript : MonoBehaviour
+{
+    public Text MyTextVariable;
+    public string Spencer;
+
+    public void MyTextScript()
+    {
+        MyTextVariable.text = Spencer; 
+    }
+    public void MyMoves()
+    {
+        MyTextVariable.text = "";
+    }
+}
