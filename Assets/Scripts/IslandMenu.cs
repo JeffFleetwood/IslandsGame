cfc6a02003b82b1bbfd2b75ebdb8fb393e8146e4
pupdog5089 <61168201+pupdog5089@users.noreply.github.com>
2020-06-18 11:24:57 -0500

    Update IslandMenu.cs

--- Assets/Scripts/IslandMenu.cs
+++ Assets/Scripts/IslandMenu.cs
@@ -9,11 +9,13 @@ public class IslandMenu : MonoBehaviour
     public Sprite[] IslandImages; //stores all the images that will give a preview of the island before it is selected
     private int index; //used to determine which item in an group/array to iterate on
 
+    //I understand that the sprite variable of the displayimage to equal the first item in the IslandImages group/array.
     public void Start() 
     {
         displayimage.sprite = IslandImages[0]; //sets the sprite variable of the displayimage to equal the first item in the IslandImages group/array
     }
 
+    //I understand that the index is used if index is greater than 0, subtracts the index value by 1 and sets the sprite variable of the displayimage to be one of the IslandImages depending on what value index is.
     public void PreviousIsland() 
     {
         if (index > 0) //used if index is greater than 0
@@ -23,6 +25,7 @@ public class IslandMenu : MonoBehaviour
         displayimage.sprite = IslandImages[index]; //sets the sprite variable of the displayimage to be one of the IslandImages depending on what value index is
     }
 
+    //I understand that the index < Island Images.Length - 1 is used if index is less than the value of IslandImages.Length - 1, the index += 1 adds 1 to the index value and the displayimage.sprite = IslandImages[index] sets the sprite variable of the displayimage to be one of the IslandImages depending on what value index is.
     public void NextIsland()
     {
         if (index < IslandImages.Length - 1) //used if index is less than the value of IslandImages.Length - 1
@@ -32,6 +35,7 @@ public class IslandMenu : MonoBehaviour
         displayimage.sprite = IslandImages[index]; //sets the sprite variable of the displayimage to be one of the IslandImages depending on what value index is
     }
 
+    //I understand that the PlayerPrefs.SetInt("IslandCount", index); saves a value that can be used to determind what island to spawn in. IslandCount is the value set by index. Index is the value setting IslandCount.
     public void SelectIsland()
     {
         PlayerPrefs.SetInt("IslandCount", index); //saves a value that can be used to determine what island to spawn in
