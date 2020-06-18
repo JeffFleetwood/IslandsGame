346386f56539eaa7ccc0c9009a9a3495069c414f
pupdog5089 <61168201+pupdog5089@users.noreply.github.com>
2020-06-18 13:52:28 -0500

    Main menu script finished.
    I completed my main menu script.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OpenMenu(GameObject openMenu)
+    {
+        openMenu.SetActive(true);
+    }
+
+    public void CloseMenu(GameObject closeMenu)
+    {
+        closeMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
