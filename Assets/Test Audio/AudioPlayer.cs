ec2699e7af4f492fb710f5f1d53fea3119aec518
murosa <54591124+murosa@users.noreply.github.com>
2020-06-22 12:55:54 -0500

    Audio clips and audio player script
    I put 2 audio clips to test out the volume settings and a script that plays and stops audio. I put them inside a folder called Test Audio.

---
+++ Assets/Test Audio/AudioPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public void PlayAudio(AudioSource aud)
+    {
+        aud.Play();
+    }
+
+    public void StopAudio(AudioSource aud)
+    {
+        aud.Stop();
+    }
+}
