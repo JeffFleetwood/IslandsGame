dc05f8ce07b52046b8b2563e86a5110be5affd17
pupdog5089 <61168201+pupdog5089@users.noreply.github.com>
2020-06-22 14:58:56 -0500

    I worked on settings menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menu.activeSelf == true)
+            {
+                menu.SetActive(false);
+                Time.timeScale = 1;
+            }
+
+            else
+            {
+                menu.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
