656c343da2f73f7d3d2a3998aea1c662a7b80540
snowyayano <60415398+snowyayano@users.noreply.github.com>
2020-06-24 11:58:07 -0500

    islands and music

---
+++ Assets/PlayOnIsland.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnIsland : MonoBehaviour
+{
+    AudioSource source;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
