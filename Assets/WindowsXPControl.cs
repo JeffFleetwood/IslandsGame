ee97b27dd16d1cc38a5d2e869a76f15112bb9029
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2020-06-24 11:47:52 -0500

    Added Windows XP Easter Egg

---
+++ Assets/WindowsXPControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsXPControl : MonoBehaviour
+{
+    public GameObject lights;
+    public Vector3 rotationModifires;
+
+    void Update()
+    {
+        lights.transform.Rotate(rotationModifires);
+    }
+}
