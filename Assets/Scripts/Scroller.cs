555b7d679b5292c0b9e5210e4164e9d7eee7b548
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2020-06-24 10:59:36 -0500

    Updated Credits to autoclose

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Events;
+
+public class Scroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public VideoPlayer player;
+    public GameObject credits;
+    public UnityEvent videoEnd;
+    bool start;
+
+    private void Start()
+    {
+        player.Play();
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, scrollSpeed * Time.deltaTime, 0);
+        
+        if (player.isPlaying)
+        {
+            start = true;
+        }
+
+        if (!player.isPlaying && start)
+        {
+            videoEnd?.Invoke();
+        }
+    }
+}
